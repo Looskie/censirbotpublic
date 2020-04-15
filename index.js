aee4794418f52edad17bb91fbd717d7548be3aa0
looskie <50378828+Looskie@users.noreply.github.com>
2020-04-15 19:05:29 -0400

    Faster and better index.js
    Made a better request the the API and what not

--- index.js
+++ index.js
@@ -2,25 +2,39 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = '';
 const PREFIX = '>';
+const API_KEY = '';
 
 // DO NOT TOUCH UNLESS RELEASED VERS
-var version = '1.0.0'
+var version = "0.2.0";
+var website = "https://www.Censirapp.com/";
 
-var website = 'https://www.CensirApp.com/'
+client.on("ready", () => {
+  client.user.setActivity("Released Beta #1.0", { type: "PLAYING" });
+  console.log("Bot is online!");
+});
 
-
-client.on('ready', () => {
-  client.user.setActivity("BETA #0.1.0", {type: "PLAYING"});
-  console.log('Bot is online!');
-})
+function calFahrenheit(temperature) {
+  let fah = Math.floor((temperature*9/5)+32);
+  return fah;
+};
 
 client.on('message', async message => {
-
   // Bunch of ifs and constants
-  if(message.author.bot) return;
-  if(message.content.indexOf(PREFIX) !== 0) return;
-  const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+  if (message.author.bot) return;
+  if (message.content.indexOf(PREFIX) !== 0) return;
+  const args = message.content
+    .slice(PREFIX.length)
+    .trim()
+    .split(/ +/g);
   const command = args.shift().toLowerCase();
+  // Uptime PARAMS
+  let totalSeconds = client.uptime / 1000;
+  let days = Math.floor(totalSeconds / 86400);
+  let hours = Math.floor(totalSeconds / 3600);
+  totalSeconds %= 3600;
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+  let uptime = `The bot has been online for ${days} days, ${hours} hours, ${minutes} minutes and ${parseFloat(seconds.toFixed(0))} seconds!`;
 
   //Commands
 
@@ -31,6 +45,10 @@ client.on('message', async message => {
 
   if(command === "website") {
     message.channel.send("Website: " + website)
+  }
+  
+   if (command === "uptime") {
+    message.channel.send(uptime);
   }
 
   //Weather Command
@@ -44,32 +62,24 @@ client.on('message', async message => {
     if (!arg) {
       return message.channel.send('Error! Please do "**{Country/Zipcode},{Country}**"')
     }
-    if(message.content.toLowerCase().includes("us")) {
-      console.log(arg);
-      const call_api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=` + arg + "&APPID=" + API_KEY + '&units=imperial');
-      const data = await call_api.json();
-      console.log(data);
-      const embed = new Discord.RichEmbed()
-      .setColor('#2C80B9')
-      .setTitle(`Weather for ${data.name}`)
-      .addField('Info:', `**Temperature**: ${data.main.temp}℉\n **High of**: ${data.main.temp_max}℉\n **Low of**: ${data.main.temp_min}℉\n **It is currently ${data.weather[0].main}**`);
-      message.channel.send({embed})
-      .catch(console.error);
-    }else {
-      console.log(arg);
-      const call_api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=` + arg + "&APPID=" + API_KEY + '&units=metric');
-      const data = await call_api.json();
-      console.log(data);
-      const embed = new Discord.RichEmbed()
-      .setColor('#2C80B9')
-      .setTitle(`Weather for ${data.name}`)
-      .addField('Info:', `**Temperature**: ${data.main.temp}℃\n **High of**: ${data.main.temp_max}℃\n **Low of**: ${data.main.temp_min}℃\n **It is currently ${data.weather[0].main}**`);
-      message.channel.send({embed})
-    };
-    if (`${data.cod}` === "404") {
-      message.channel.send('test');
+  try {
+    const call_api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=` + arg + "&APPID=" + API_KEY + "&units=metric");
+    const data = await call_api.json();
+    if (data.sys.country === 'US') {
+      temperature = calFahrenheit(data.main.temp)
+      temp_max = calFahrenheit(data.main.temp_max)
+      temp_min = calFahrenheit(data.main.temp_min)
+      const embed = new Discord.RichEmbed().setColor("#2C80B9").setTitle(`Weather for ${data.name}`).addField("Weather:", `**Temperature**: ${temperature} ℉\n **High of**: ${temp_max} ℉\n **Low of**: ${temp_min} ℉\n **Description**: ${data.weather[0].main}`);
+      message.channel.send({ embed });
+    } else {
+      const embed = new Discord.RichEmbed().setColor("#2C80B9").setTitle(`Weather for ${data.name}`).addField("Weather:", `**Temperature**: ${data.main.temp} ℃\n **High of**: ${data.main.temp_max} ℃\n **Low of**: ${data.main.temp_min} ℃\n **Description**: ${data.weather[0].main}`);
+      message.channel.send({ embed });
     }
-  };
+  } catch(err) {
+    message.channel.send("Error! Try checking the way you spelled or inputed your city/country!")
+    console.log(`The args put for this error are, ${arg}`);
+  }
+};
 });
 
 client.login(token);
